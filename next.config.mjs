c51b925a43ed253dfa05cfe670e774e937183da8
a9261020 <fish840212@yahoo.com.tw>
2024-08-19 11:29:07 +0800

    change config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+  assetPrefix: 'https://a9261020.github.io/gh-practice/',
+};
 
 export default nextConfig;
